acd58ead5f4d0830af7b08b8e434ccedf8241624
徐涛 <3264992329@qq.com>
2024-12-30 17:06:30 +0800

    spring cloud GeteWay 3大核心（路由，断言，过滤）3

--- cloud-geteway9527/src/main/java/com/atguigu/cloud/mygateway/MyGlobalFilter.java
+++ cloud-geteway9527/src/main/java/com/atguigu/cloud/mygateway/MyGlobalFilter.java
@@ -1,4 +1,42 @@
 package com.atguigu.cloud.mygateway;
 
-public class MyGlobalFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+
+    public final static String BEGIN_VISIT_TIME = "beginVisitTime";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //记录访问接口开始时间
+        exchange.getAttributes().put(BEGIN_VISIT_TIME, System.currentTimeMillis());
+        //返回统计的各个结果给后台
+        return chain.filter(exchange).then(Mono.fromRunnable(()->{
+            Long beginVisitTime = exchange.getAttribute(BEGIN_VISIT_TIME);
+            if(beginVisitTime != null){
+                log.info("访问接口主机："+exchange.getRequest().getURI().getHost());
+                log.info("访问接口端口："+exchange.getRequest().getURI().getPort());
+                log.info("访问接口URL："+exchange.getRequest().getURI().getPath());
+                log.info("访问接口URL后面的参数："+exchange.getRequest().getURI().getRawQuery());
+                log.info("访问接口时长："+(System.currentTimeMillis()-beginVisitTime)+"毫秒");
+                log.info("=================分割线======================");
+                System.out.println();
+            }
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
